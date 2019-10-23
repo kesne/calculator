64824c1c027976b76cfc383cf7f393662653a246
Student <example@example.com>
2019-10-23 13:02:59 -0700

    Changed page to include +,-,*,/ in the owen collumn, and display result

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,46 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("enter a number"));
-var number2 = parseInt(prompt("enter another number"));
-alert (add(number1,number2));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+   var number1 = parseInt($("#add1").val());
+   var number2 = parseInt($("#add2").val());
+   var result = add(number1, number2);
+   $("#output").text(result);
+ });
+ $("form#subtract").submit(function(event) {
+  event.preventDefault();
+  var number1 = parseInt($("#sub1").val());
+  var number2 = parseInt($("#sub2").val());
+  var result = subtract(number1, number2);
+  $("#output").text(result);
+});
+$("form#multiply").submit(function(event) {
+ event.preventDefault();
+ var number1 = parseInt($("#mult1").val());
+ var number2 = parseInt($("#mult2").val());
+ var result = multiply(number1, number2);
+ $("#output").text(result);
+});
+$("form#divide").submit(function(event) {
+ event.preventDefault();
+ var number1 = parseInt($("#div1").val());
+ var number2 = parseInt($("#div2").val());
+ var result = divide(number1, number2);
+ $("#output").text(result);
+});
+
+});
