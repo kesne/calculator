38d577f1df2356c6ca75b4e0c61dcb81222baccd
Student <example@example.com>
2019-10-23 16:39:58 -0700

    begin work toward pressable calculator

--- js/scripts.js
+++ js/scripts.js
@@ -14,34 +14,20 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-$(document).ready(function() {
-  $("form#add").submit(function(event) {
-   event.preventDefault();
-   var number1 = parseInt($("#add1").val());
-   var number2 = parseInt($("#add2").val());
-   var result = add(number1, number2);
-   $("#output").text(result);
- });
- $("form#subtract").submit(function(event) {
-  event.preventDefault();
-  var number1 = parseInt($("#sub1").val());
-  var number2 = parseInt($("#sub2").val());
-  var result = subtract(number1, number2);
-  $("#output").text(result);
-});
-$("form#multiply").submit(function(event) {
- event.preventDefault();
- var number1 = parseInt($("#mult1").val());
- var number2 = parseInt($("#mult2").val());
- var result = multiply(number1, number2);
- $("#output").text(result);
-});
-$("form#divide").submit(function(event) {
- event.preventDefault();
- var number1 = parseInt($("#div1").val());
- var number2 = parseInt($("#div2").val());
- var result = divide(number1, number2);
- $("#output").text(result);
-});
+var number1 = "";
+var number2 = "";
+var operation = "";
 
+$(document).ready(function() {
+  $("div.key").click(function() {
+    if ( (this.id === "+") || (this.id === "-") || (this.id === "*") || (this.id === "/") ) {
+      return;
+    } else if (this.id === "clr") {
+    } else {
+    number1 = number1 + this.id;
+    $("h3#display").text(number1);
+    }
+  });
+
+  // $("h3#display").text(number1);
 });
