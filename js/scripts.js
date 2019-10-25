87126fa3774f99c7ffef24426c5a0f76fae79951
Jordan Gensler <jgensler@netflix.com>
2019-10-24 20:53:14 -0700

    Make more religious

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,69 @@
-var result = "";
-var hasOp = 0;
+var result = '';
+var hasOp = false;
 
 function doMath(string) {
-  var array = string.split(' ');
-  var num1 = parseFloat(array[0]);
-  var num2 = parseFloat(array[2]);
-  var operator = array[1];
+    var array = string.split(' ');
+    var num1 = parseFloat(array[0]);
+    var num2 = parseFloat(array[2]);
+    var operator = array[1];
 
-  if (operator === "+") {
-    return (num1 + num2);
-  } else if (operator === "-") {
-    return (num1 - num2);
-  } else if (operator === "*") {
-    return (num1 * num2);
-  } else if (operator === "÷") {
-    return (num1 / num2);
-  }
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '÷':
+            return num1 / num2;
+    }
 }
 
-function opCheck(keyPress) {
-  if ((keyPress === " + ") || (keyPress === " - ") || (keyPress === " * ") || (keyPress === " ÷ ")) {
-    return true;
-  } else {
-    return false;
-  };
-};
+function doMath(str) {
+    return eval(str);
+}
 
-function pressLogic(keyPress) {
-  if (keyPress === "clr") {
-    result = "";
-    hasOp = 0;
-  } else if (keyPress === "=") {
-    result = doMath(result);
-    hasOp = 0;
-  } else if (opCheck(keyPress) && result === "") {
-    result = "";
-  } else if (hasOp === 0 && opCheck(keyPress)) {
-    result += keyPress;
-    hasOp = 1;
-  } else if (hasOp === 1 && opCheck(keyPress)) {
-    console.log("Can't run two operators!");
-  } else {
-    result += keyPress;
-  }
+function opCheck(node) {
+    if ($(node).attr('data-op')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function pressLogic(node) {
+    const keyPress = node.id;
+
+    if (keyPress === 'clr') {
+        result = '';
+        // hasOp = false;
+    } else if (keyPress === '=') {
+        result = doMath(result);
+        if (result === 666) {
+            goToHell();
+        }
+        // hasOp = false;
+    } else if (opCheck(node) && result === '') {
+        result = '';
+    } else if (!hasOp && opCheck(node)) {
+        result += ' ' + $(node).attr('data-op') + ' ';
+        // hasOp = true;
+    } else if (hasOp && opCheck(node)) {
+        // console.log("Can't run two operators!");
+    } else {
+        result += keyPress;
+    }
+}
+
+function goToHell() {
+    $('#hell').css('opacity', '0.8');
+    $('#calc').css('transform', 'rotateZ(180deg)');
+    document.getElementById('satan-laughs').play();
 }
 
 $(document).ready(function() {
-  $("div.key").click(function() {
-    pressLogic(this.id);
-    $("h3#display").text(result);
-  });
+    $('div.key').click(function(e) {
+        pressLogic(e.currentTarget);
+        $('h3#display').text(result);
+    });
 });
